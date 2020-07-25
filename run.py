15e1387f1e1d160f63d383735ea5078af27968d5
UiTae Kim <taejja09@gmail.com>
2020-07-25 21:19:19 +0900

    modified epochs selection to use system arguments

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ path = sys.argv[2]
 # model size
 max_size = int(sys.argv[1])
 layers = [2 ** x for x in range(int(math.log2(max_size)), 6, -1)]
+epochs = int(sys.argv[3])
 
 # data
 train, cv, test, raw = load(path)
@@ -23,5 +24,7 @@ train, cv, test, raw = load(path)
 # model
 create_checkpoint_dir(str(layers[0]))
 network, error, output, save = model(layers, X_train.shape[1])
-pred = run(network, error, output, save, X_train, y_train, X_cv, y_cv, X_test, y_test)
+pred = run(network, error, output, save,
+           X_train, y_train, X_cv, y_cv, X_test, y_test,
+           num_epochs=epochs)
 pred = scaler.inverse_transform(np.concatenate((X_test, pred), axis=1))[:, -1]
